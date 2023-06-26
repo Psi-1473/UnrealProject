291dac4e1d21acf2f3c266757da93611fd73a4aa
PeoshPeush <PeoshPeush@gmail.com>
2023-06-26 23:27:24 +0900

    .

---
+++ UnrealProj/Source/UnrealProj/Skills/EffectActor/Player/SEA_Bow_First.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../SkillEffectActor.h"
+#include "SEA_Bow_First.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJ_API ASEA_Bow_First : public ASkillEffectActor
+{
+	GENERATED_BODY()
+	
+};
