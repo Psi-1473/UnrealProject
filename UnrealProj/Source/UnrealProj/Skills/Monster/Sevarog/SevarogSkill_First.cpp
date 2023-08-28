d3e5b04df3ecf9facfe0f0e9e1a2ee547f896de5
PeoshPeush <PeoshPeush@gmail.com>
2023-08-29 03:06:31 +0900

    잡버그 수정

--- UnrealProj/Source/UnrealProj/Skills/Monster/Sevarog/SevarogSkill_First.cpp
+++ UnrealProj/Source/UnrealProj/Skills/Monster/Sevarog/SevarogSkill_First.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed705f28dba7c1c3f7cfa51a051f63bdc8d8af66932a11d6b8d156b5fadf695d
+oid sha256:673ab7fe3e22f6ce7c99ebca8cf0828fee71381798aceb5f4db126ac9f846af2
 size 4424
--- UnrealProj/Source/UnrealProj/Skills/Monster/Sevarog/SevarogSkill_Fourth.cpp
+++ UnrealProj/Source/UnrealProj/Skills/Monster/Sevarog/SevarogSkill_Fourth.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85fca19cdb36a35772364ea58fe99d1426d7bd7dae0c1f611766268818948315
+oid sha256:8913eeab066045da663a7c9d8c3748602e26afa5eb1bfb3c7b2b17b8b0fb33af
 size 6227
