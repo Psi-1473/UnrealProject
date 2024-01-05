95a201dc03d32b55fa0b04526811234bcda9a6c1
PeoshPeush <PeoshPeush@gmail.com>
2024-01-05 21:29:32 +0900

    부활 UI 추가

---
+++ UnrealProj/Source/UnrealProj/Widgets/Popup/Widget_Revive.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_Revive.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJ_API UWidget_Revive : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
